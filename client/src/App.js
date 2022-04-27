d8004b8ad4746aa0d52a8c774b300aad1c57d206
mohamd hamdy <74797199+mohamed0-1@users.noreply.github.com>
2022-04-27 20:53:20 +0200

    starter react project and header component

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+    <div className="layout">
+      <Header/>
+      <main>content</main>
+      <footer>footer</footer>
+    </div>
+    </>
+  );
+}
+
+export default App;
