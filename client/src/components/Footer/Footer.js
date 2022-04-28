8fb1267afe00c2fa49526d382d221e7bf7c174bd
mohamd hamdy <74797199+mohamed0-1@users.noreply.github.com>
2022-04-28 13:11:22 +0200

    footer-component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+ import './Footer.scss'
+ function Footer() {
+  return (
+    <footer>
+        &copy; copyright Mohamed Hamdy 2022
+    </footer>
+  )
+}
+
+export default Footer
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../css/Header.css'
+import './Header.scss'
 function Header() {
   return( 
     <header>
