c55c2c8ec207931eb8d64ff8084589e64cdd1166
mohamd hamdy <74797199+mohamed0-1@users.noreply.github.com>
2022-06-05 15:56:21 +0200

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,14 @@
 /* eslint-disable no-undef */
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Cart from './components/Cart/Cart';
 import Filter from './components/Filter/Filter';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header'
 import Products from './components/Products/Products';
-import data from './data.json'
 import {Provider} from 'react-redux'
 import store from './store/store'
-
-// import {words} from "./words"
-
 function App() {
-  // console.log(data)
-  
-  const [products, setProducts] = useState(data)
-  // console.log(products)
-
-  const [sort, setSort ] =useState(" ");
-  const [size, setSize ] =useState("");
-  const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cartItems')) || []);
-
-  const handleFilterBySize = (e) => {
-    setSize(e.target.value)
-    if (e.target.value == "ALL"){
-      setProducts(data)
-    }else{
-    let productsClone= [...products]
-    let newProducts = productsClone.filter(p => p.sizes.indexOf(e.target.value) != -1)
-    setProducts(newProducts)
-    }
-  }
-
-  const handleFilterByOrder = (e) => {
-    let order = e.target.value
-    setSort(order)
-    let productsClone= [...products]
-    let newProducts = productsClone.sort(function(a,b){
-      if (order == "lowest"){
-        return a.price - b.price
-      }else if(order == "highest"){
-        return b.price - a.price
-      }else {
-        return a.id < b.id ? 1 : -1
-      }
-    });
-   setProducts(newProducts)
-  }
-
-  const addToCart =(product) =>{
-    const cartItemsClone = [...cartItems];
-    let isProductExist = false;
-    cartItemsClone.forEach(p => {
-        if(p.id == product.id) {
-            p.qty++;
-            isProductExist= true;
-        }
-    })
-    if(!isProductExist) {
-      cartItemsClone.push({...product, qty: 1})
-  }
-    setCartItems(cartItemsClone);
-  }
-  
-  // useEffect(() => {
-  //   localStorage.setItem('cartItems', JSON.stringify(cartItems))
-  // }, [cartItems])
-
-  const removeFromCart =(product) =>{
-    const cartItemsClone =[ ...cartItems];
-    setCartItems(cartItemsClone.filter(p => p.id !== product.id))
-
-  }
-
+ 
   return (
     <Provider store={store}>
       <div className="layout">
@@ -81,15 +17,9 @@ function App() {
           <div className='wrapper'>
              <Products /> 
              {/* <Filter /> */}
-             <Filter
-                productNumber={products.length}
-                size={size}
-                sort={sort}
-                handleFilterBySize ={handleFilterBySize}
-                handleFilterByOrder={handleFilterByOrder}/>
-                
+             <Filter/>
           </div>
-          <Cart cartItems={cartItems} removeFromCart={removeFromCart}/>
+          <Cart />
         </main>
       <Footer/> 
     </div>
