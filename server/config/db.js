ca0340beb2b5490a728ac421dba8a1e2b1f532ad
mohamd hamdy <74797199+mohamed0-1@users.noreply.github.com>
2022-07-20 16:01:15 +0200

    add config

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectionString='mongodb://localhost/react-shopping-cart';
 function runDB(){
-    mongoose.connect(connectionString, {useNewUrlParser: true , useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGO_URL || connectionString, {useNewUrlParser: true , useUnifiedTopology: true})
     .then(res => console.log("connection Done"))
 
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -5,15 +6,27 @@ const productRouter = require('./routes/routes');
 const orderRouter = require('./routes/orderRoutes');
 const runDB = require('./config/db');
 
+console.log(process.env.NODE_ENV);
 const app = express();
 app.use(bodyParser.json());
-app.use('/', productRouter)
-app.use('/', orderRouter)
 
 runDB()
 
- 
 
-app.listen(5001, ()=>{
+app.use('/', productRouter)
+app.use('/', orderRouter)
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('public'));
+
+    app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"))
+} else {
+    app.get('/' , (req,res) => res.send("API Runnnning"))
+}
+
+ 
+const PORT = process.env.PORT ;
+app.listen( PORT || 5001, ()=>{
     console.log("Running in port 5001")
 })
